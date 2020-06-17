08ead0f0b6999627d7d21a7f1b68fcc280c3ff0f
Anand Shende <anands@smartek21.com>
2020-06-17 14:15:14 +0530

    Adding SSL Cert from config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   listenIp: '0.0.0.0',
   listenPort: 3000,
-  // sslCrt: '/etc/ssl/certs/ssl-cert-snakeoil.pem',
-  // sslKey: '/etc/ssl/private/ssl-cert-snakeoil.key',
+  sslCrt: '/etc/ssl/certs/nginx-selfsigned.crt',
+  sslKey: '/etc/ssl/private/nginx-selfsigned.key',
   mediasoup: {
     // Worker settings
     worker: {
