7c1e8e442db699a8d661935164f958d2c98ed538
Anand Shende <anands@smartek21.com>
2020-06-17 17:14:57 +0530

    Removed SSL..

--- server.js
+++ server.js
@@ -47,16 +47,17 @@ async function runExpressApp() {
 }
 
 async function runWebServer() {
-  const { sslKey, sslCrt } = config;
-  if (!fs.existsSync(sslKey) || !fs.existsSync(sslCrt)) {
-    console.error('SSL files are not found. check your config.js file');
-    process.exit(0);
-  }
-  const tls = {
-    cert: fs.readFileSync(sslCrt),
-    key: fs.readFileSync(sslKey),
-  };
-  webServer = https.createServer(tls, expressApp);
+  // const { sslKey, sslCrt } = config;
+  // if (!fs.existsSync(sslKey) || !fs.existsSync(sslCrt)) {
+  //   console.error('SSL files are not found. check your config.js file');
+  //   process.exit(0);
+  // }
+  // const tls = {
+  //   cert: fs.readFileSync(sslCrt),
+  //   key: fs.readFileSync(sslKey),
+  // };
+  // webServer = https.createServer(tls, expressApp);
+  webServer = https.createServer(expressApp);
   webServer.on('error', (err) => {
     console.error('starting web server failed:', err.message);
   });
